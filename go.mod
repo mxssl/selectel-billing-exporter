1bae46e36dc7e8e0a2bdd597d9f6e6f996ecf8aa
mxssl <m.susula@gmail.com>
2021-06-13 00:59:38 +0300

    test

--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module github.com/mxssl/selectel_billing_exporter
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jarcoal/httpmock v1.0.8
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/common v0.29.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
 )
 
---
+++ main_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectelBillingRequest(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	testdata, err := ioutil.ReadFile("testdata/SelectelBillingRespExample.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	httpmock.RegisterResponder("GET", "https://my.selectel.ru/api/v3/billing/balance",
+		httpmock.NewStringResponder(200, string(testdata)))
+
+	expected := selectelBillingResponse{Status: "success", Data: struct {
+		Currency  string "json:\"currency\""
+		IsPostpay bool   "json:\"is_postpay\""
+		Discount  int    "json:\"discount\""
+		Primary   struct {
+			Main  int "json:\"main\""
+			Bonus int "json:\"bonus\""
+			VkRub int "json:\"vk_rub\""
+			Ref   int "json:\"ref\""
+			Hold  struct {
+				Main  int "json:\"main\""
+				Bonus int "json:\"bonus\""
+				VkRub int "json:\"vk_rub\""
+			} "json:\"hold\""
+		} "json:\"primary\""
+		Storage struct {
+			Main       int         "json:\"main\""
+			Bonus      int         "json:\"bonus\""
+			VkRub      int         "json:\"vk_rub\""
+			Prediction interface{} "json:\"prediction\""
+			Debt       int         "json:\"debt\""
+			Sum        int         "json:\"sum\""
+		} "json:\"storage\""
+		Vpc struct {
+			Main       int         "json:\"main\""
+			Bonus      int         "json:\"bonus\""
+			VkRub      int         "json:\"vk_rub\""
+			Prediction interface{} "json:\"prediction\""
+			Debt       int         "json:\"debt\""
+			Sum        int         "json:\"sum\""
+		} "json:\"vpc\""
+		Vmware struct {
+			Main       int         "json:\"main\""
+			Bonus      int         "json:\"bonus\""
+			VkRub      int         "json:\"vk_rub\""
+			Prediction interface{} "json:\"prediction\""
+			Debt       int         "json:\"debt\""
+			Sum        int         "json:\"sum\""
+		} "json:\"vmware\""
+	}{Currency: "rub", IsPostpay: false, Discount: 0, Primary: struct {
+		Main  int "json:\"main\""
+		Bonus int "json:\"bonus\""
+		VkRub int "json:\"vk_rub\""
+		Ref   int "json:\"ref\""
+		Hold  struct {
+			Main  int "json:\"main\""
+			Bonus int "json:\"bonus\""
+			VkRub int "json:\"vk_rub\""
+		} "json:\"hold\""
+	}{Main: 10000, Bonus: 10000, VkRub: 0, Ref: 0, Hold: struct {
+		Main  int "json:\"main\""
+		Bonus int "json:\"bonus\""
+		VkRub int "json:\"vk_rub\""
+	}{Main: 0, Bonus: 0, VkRub: 0}}, Storage: struct {
+		Main       int         "json:\"main\""
+		Bonus      int         "json:\"bonus\""
+		VkRub      int         "json:\"vk_rub\""
+		Prediction interface{} "json:\"prediction\""
+		Debt       int         "json:\"debt\""
+		Sum        int         "json:\"sum\""
+	}{Main: 203005, Bonus: 0, VkRub: 0, Prediction: interface{}(nil), Debt: 0, Sum: 203005}, Vpc: struct {
+		Main       int         "json:\"main\""
+		Bonus      int         "json:\"bonus\""
+		VkRub      int         "json:\"vk_rub\""
+		Prediction interface{} "json:\"prediction\""
+		Debt       int         "json:\"debt\""
+		Sum        int         "json:\"sum\""
+	}{Main: 11250838, Bonus: 12345, VkRub: 0, Prediction: interface{}(nil), Debt: 0, Sum: 11150838}, Vmware: struct {
+		Main       int         "json:\"main\""
+		Bonus      int         "json:\"bonus\""
+		VkRub      int         "json:\"vk_rub\""
+		Prediction interface{} "json:\"prediction\""
+		Debt       int         "json:\"debt\""
+		Sum        int         "json:\"sum\""
+	}{Main: 10000, Bonus: 10000, VkRub: 0, Prediction: interface{}(nil), Debt: 0, Sum: 20000}}}
+
+	actual := selectelBillingResponse{}
+
+	if err := getSelectelBilling(&actual); err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, expected, actual)
+}
